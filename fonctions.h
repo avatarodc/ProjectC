d62952803c0dbbf8db2de5402623a404c19bd69c
avatarodc <odcmamadougueye@gmail.com>
2024-03-07 13:43:49 +0000

    connexionV2

--- fonctions.h
+++ fonctions.h
@@ -1,6 +1,13 @@
 #ifndef FONCTIONS_H_INCLUDED
 #define FONCTIONS_H_INCLUDED
 
+
+#define MAX_APPRENANTS 100
+#define MAX_CLASSES 10
+#define MAX_MATRICULE_LENGTH 20
+#define MAX_DATE_LENGTH 20
+
+
 typedef struct {
     char username[50];
     char password[50];
@@ -8,25 +15,19 @@ typedef struct {
 
 
 typedef struct {
-    char etat[10];
-    char prenom[50];
     char nom[50];
-    int id;
-    char username[50];
-    char password[50];
-    char classe[50];
-    int nbr_absence;
-    int cumul_retard;
-    char matricule[50]; // Nouveau champ pour le matricule de l'étudiant
+    char prenom[50];
+    char matricule[MAX_MATRICULE_LENGTH];
+    char classe[20];
 } Apprenant;
 
+// Structure pour représenter une liste de présence
 typedef struct {
-    int id_apprenant;
-    char classe[50];
-    char date_heure[20];
-    int present; // 1 pour présent, 0 pour absent
-} Absence;
-
+    char date[MAX_DATE_LENGTH];
+    char classe[20];
+    int nb_apprenants;
+    char matricules_present[MAX_APPRENANTS][MAX_MATRICULE_LENGTH];
+} ListePresence;
 
 
 
@@ -36,24 +37,17 @@ int verifier_connexion_admin(const char* nom_fichier, Utilisateur utilisateur);
 int verifier_connexion_apprenant(const char* nom_fichier, Utilisateur utilisateur);
 void afficher_menu_admin(const char* nom_fichier, Utilisateur user);
 void afficher_menu_apprenant(const char* nom_fichier, Utilisateur user);
+int contient_seulement_espaces(const char *chaine);
 void gerer_menu_admin(const char* nom_fichier, Utilisateur user);
 void gerer_menu_apprenant(const char* nom_fichier, Utilisateur user);
-void lister_classes(const char* nom_fichier);
-void lister_etudiants_classe(const char* nom_fichier, const char* classe);
-void marquer_presence_apprenant(const char* nom_fichier, int id);
-void afficher_absences(const char* nom_fichier, int id_apprenant);
-void marquer_absence(const char* nom_fichier, int id_apprenant, const char* date_heure);
-void saisie_mot_de_passe(char *password, int max_length);
-void connexion_admin(const char* nom_fichier);
-void lister_classes(const char* nom_fichier);
-void afficher_presences_classe(const char* nom_fichier, const char* classe) ;
-void choisir_classe() ;
-void afficher_liste_presences(const char* nom_fichier, const char* classe) ;
-int est_dans_classe(int id, const char* classe);
-int est_present(int id, const char* nom_fichier);
-int contient_seulement_espaces(const char *chaine);
-void afficher_etudiants_presence_classe(const char* nom_fichier, const char* classe) ;
-void marquer_presence(const char* nom_fichier, int id_apprenant, const char* classe, int present);
+
+
+int charger_apprenants(Apprenant *apprenants, const char *nom_fichier);
+void sauvegarder_apprenants(Apprenant *apprenants, int nb_apprenants, const char *nom_fichier);
+void charger_presence(ListePresence *presence, const char *nom_fichier);
+void sauvegarder_presence(ListePresence *presence, const char *nom_fichier);
+void marquer_present(ListePresence *presence, Apprenant *apprenants, int nb_apprenants);
+void afficher_presence(ListePresence presence);
 
 
 #endif /* FONCTIONS_H */
